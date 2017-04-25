cbba5d0afe051a375d2b5b4aabb39a3c2d933510
SwDev08 <SwDev08@fasbaldoyle.ie>
2017-04-25 15:29:03 +0100

    First comit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created';
+
+  constructor() { 
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
+    this.allowNewServer = false;
+  }
+}
---
+++ src/app/worningAlert/worningAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-worningAlert',
+    templateUrl: './worningAlert.component.html',
+    styleUrls: ['./worningAlert.component.css']
+})
+
+export class worningAlertComponent{
+
+}
