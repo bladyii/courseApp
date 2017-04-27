c5a740fe198c70c7baf4e87080be6b133a40a670
SwDev08 <SwDev08@fasbaldoyle.ie>
2017-04-27 14:04:51 +0100

    final

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,8 +11,11 @@ export class ServersComponent implements OnInit {
   serverName='Enter Server Name';
   userName='';
   updateUserName = false;
-
-
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 2'];
+  passwords = [];
+  detailsVisible= false;
+  dounatNumber = 1;
   constructor() { 
 
     setTimeout(() => {
@@ -27,6 +30,9 @@ export class ServersComponent implements OnInit {
   onCreateServer(){
     this.serverCreationStatus = 'Server was created! Name is: '+this.serverName;
     this.allowNewServer = false;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+
   }
 
   onUpdateServerName(event: any){
@@ -43,5 +49,18 @@ export class ServersComponent implements OnInit {
         this.updateUserName = true;
         else
         this.updateUserName = false;   
+  }
+
+  toggleDetails(){
+    this.detailsVisible = this.detailsVisible ? false : true;
+    this.passwords.push(this.passwords.length + 1);
+   
+  }
+
+  getLiClass(){
+   
+    
+    return this.passwords.length + 1 >= 5 ? true : false;
+    
   }
 }
